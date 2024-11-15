591a1981f9a5a22bf81c4438dc7a14da8975c4c6
LeThiQuynhNhi1801 <LeThiQuynhNhi1801@gmail.com>
2024-11-15 13:30:42 +0700

    nhii

---
+++ src/main/java/film/api/DTO/request/HistoryRequestDTO.java
@@ -0,0 +1,29 @@
+package film.api.DTO.request;
+
+import film.api.models.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HistoryRequestDTO {
+    Long id;
+    private Double WatchedTime;
+    private User user;
+
+
+    private Integer Rate;
+
+
+    private LocalDateTime HistoryView;
+
+    private String device;
+
+    private String weather;
+
+    private Timestamp time;
+}
---
+++ src/main/java/film/api/exception/FileEmptyException.java
@@ -0,0 +1,9 @@
+//package film.api.exception;
+//
+//import film.api.exception.NotFoundException;
+//
+//public class FileEmptyException extends NotFoundException {
+//    public FileEmptyException() {
+//        setMessage("File is empty");
+//    }
+//}
